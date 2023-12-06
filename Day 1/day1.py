aa56197bcc62e84994dbb168bc46ec2c4c32f52e
Daniel Yang <danielyangdev@gmail.com>
2023-12-06 14:25:31 -0800

    Day 2

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,13 +1,25 @@
-def calibrationSum(fileName = "test.txt"):
+def calibrationSum():
     total = 0
-    file = open(fileName, "r")
-    # file = open("Advent-of-Code-2023/Day_1/input.txt")
-    
+    file = open("/Users/danielyang/VSCodeProjects/Advent of Code 2023/Advent-of-Code-2023/Day 1/test.txt", "r")
     
     for value in file:
-        print(value)
-    
-
+        firstNum = -1
+        lastNum = 0
+        for letter in range(len(value)):
+            if firstNum == -1 and value[letter].isdigit() == True:
+                firstNum = value[letter]
+            elif value[letter].isdigit() == True:
+                lastNum = value[letter] 
+                
+        if lastNum == 0:
+            lastNum = int(str(firstNum) + str(firstNum))
+            
+        print("TOTAL")
+        total = int(firstNum) + int(lastNum)
+        # total = total + int(firstNum) + int(lastNum)
+        continue
+        
+    print(total)
     file.close()
     
 def main():
---
+++ Day 2/day2.py
@@ -0,0 +1,14 @@
+def findSum(red, green, blue):
+    redCount = 0
+    greenCount = 0
+    blueCount = 0
+
+    file = open("/Users/danielyang/VSCodeProjects/Advent of Code 2023/Advent-of-Code-2023/Day 2/input.txt", "r")
+    
+    #skip line if a single rbg value >
+
+def main():
+    findSum(12, 13, 14)
+    
+if __name__ == "__main__":
+    main()
